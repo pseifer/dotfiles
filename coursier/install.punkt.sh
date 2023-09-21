8888ff4ec13fd44b0e6edbaa068138354a76c136
Philipp Seifer <github@seifer.me>
2023-09-21 19:28:50 +0200

    Fix coursier and setup

--- coursier/install.punkt.sh
+++ coursier/install.punkt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 punckt_curl_gzip "https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz" "cs"
-chmod +x "$PUNCKT_STORAGE/cs"
-"$PUNCKT_STORAGE/cs" "setup"
+chmod +x "$PUNKT_STORAGE/cs"
+"$PUNKT_STORAGE/cs" "setup"
 
