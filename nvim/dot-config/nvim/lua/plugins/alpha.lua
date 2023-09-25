14e4fd5d09e6b4961d66e620121cd3bd72600be6
Philipp Seifer <github@seifer.me>
2023-09-25 22:46:08 +0200

    Update alpha menu

--- nvim/dot-config/nvim/lua/plugins/alpha.lua
+++ nvim/dot-config/nvim/lua/plugins/alpha.lua
@@ -8,11 +8,11 @@ return {
     local dashboard = require('alpha.themes.dashboard')
     local logo = [[
     ▌▀██████████████████████████████████████████████▌▀▐             ▄▓         
-       ███████████▒████▒███▒██▒▒████▒▒███████████▌░▀ ░            ■▀░   ▄░   ▄▐
-      |▐▓█▀▐█████▒▒████▒▒█▒▒█▒▒▒▒██▒▒▒▒█████▓▓▓█▐■                ▄▒  ▀     ░
-        ░▐▌░▐▌███▒▒████▒▒█▒▒█▒▒█▒▒▒▒█▒▒██▓▓▓█▓▓▌   ░             ▀   ░░
-▓        ▐  ▀ ▌▓██▒▒██▒▒██▒▒▓▒▒██▒▒▓▓▒▒▓▓▓▓░░▀▌▐
- ▒          ▐  ▐▒██▒▒▒▒▓▓▓▒▓▓▓▒▓▓▓▓▓▓▓▓▓▓▓▒▒░      ▬       ▄▄▬▬ -·  ·   ·─--- ·
+       ███████████ ████ ███ ██  ████  ███████████▌░▀ ░            ■▀░   ▄░   ▄▐
+      |▐▓█▀▐█████  ████  █  █    ██    █████▓▓▓█▐■                ▄▒  ▀     ░
+        ░▐▌░▐▌███  ████  █  █  █    █  ██▓▓▓█▓▓▌   ░             ▀   ░░
+▓        ▐  ▀ ▌▓██  ██  ██  ▓  ██  ▓▓  ▓▓▓▓░░▀▌▐
+ ▒          ▐  ▐▒██    ▓▓▓ ▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓▒▒░      ▬       ▄▄▬▬ -·  ·   ·─--- ·
   ┐          ░  ▀▓▓██▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▀▐ ▀▐      ▓     /▀
  ░ ▄            ▀▐▀▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▌▓ ▌         ░░    .  - ══─--
   ▒▒▓■             ▐ ▀▓▓▀▒▒▒▀▐▒▌▐▒▀ ▌ ■             ▄╝
@@ -34,12 +34,12 @@ return {
     ]]
     dashboard.section.header.val = vim.split(logo, '\n')
     dashboard.section.buttons.val = {
-      dashboard.button( "e", "  ❯ New file" , ":ene <BAR> startinsert <CR>"),
-      dashboard.button( "f", "  ❯ Find files", ":Telescope find_files<CR>"),
-      dashboard.button( "p", "  ❯ Find projects", ":Telescope repo list bin=/usr/bin/fdfind<CR>"),
-      dashboard.button( "r", "  ❯ Find recent", ":Telescope oldfiles<CR>"),
-      dashboard.button( "s", "  ❯ Settings", ":e $MYVIMRC<CR>"),
-      dashboard.button( "q", "   ❯ Quit", ":qa<CR>"),
+      dashboard.button( "e", "    New file" , ":ene <BAR> startinsert <CR>"),
+      dashboard.button( "f", "    Find files", ":Telescope find_files<CR>"),
+      dashboard.button( "p", "    Find projects", ":Telescope repo list bin=/usr/bin/fdfind<CR>"),
+      dashboard.button( "r", "    Find recent", ":Telescope oldfiles<CR>"),
+      dashboard.button( "s", "    Settings", ":e $MYVIMRC<CR>"),
+      dashboard.button( "q", "    Quit", ":qa<CR>"),
     }
     alpha.setup(dashboard.opts)
   end
