82ef1a2fcf6d66b2990a81ab3729731e353b6811
Philipp Seifer <github@seifer.me>
2023-09-28 15:48:52 +0200

    Update LaTeX conifg

--- nvim/dot-config/nvim/lua/plugins/init.lua
+++ nvim/dot-config/nvim/lua/plugins/init.lua
@@ -1,10 +1,22 @@
 return {
-   -- Core
+   -- NVIM interface to tree-sitter.
    'nvim-treesitter/nvim-treesitter',
+   
+   -- Syntax highlighting for many languages.
    'sheerun/vim-polyglot',
-   'preservim/nerdtree',
+
+   -- Small plugin for seamless navigation between
+   -- VIM windows and TMUX panes.
    'christoomey/vim-tmux-navigator',
 }
+
+-- TODO
+-- Setup general
+--    spell checker + hotkeys
+--    completion engine + hotkeys (vim-cmp?)
+--    snippet engine + hotkeys (vsnip?)
+-- Setup hotkeys
+--    org-mode
 
 -- Further reading and examples:
 -- https://github.com/edr3x/.dotfiles/
