8b8c6e6f39d73ccbc90b03f5a3848ce8abcb70bc
Philipp Seifer <github@seifer.me>
2023-11-01 08:16:45 +0100

    Add links to shell/install

--- shell/install.punkt.sh
+++ shell/install.punkt.sh
@@ -9,3 +9,6 @@ pip install bashate
 
 # Install zoxide.
 punkt_curl_bash "https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh"
+
+# On WSL, add links to Org and Projects on Windows Host.
+grep -q Microsoft /proc/version && ln -s /mnt/d/Cloud/Org Org && ln -s /mnt/d/Projects Projects
